88849878c71dba57c000954c8f75778b822044dc
ae.dashkov <water.universe@yandex.ru>
2023-12-18 09:17:46 +0300

    Добавьте файлы проекта.

---
+++ PowerShellApp/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+using System.Diagnostics;
+
+namespace PowerShellApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            var powerShell = PowerShell.Create();
+            List<PSObject> processes = powerShell.AddCommand("Get-Process").Invoke().ToList();
+
+            foreach (PSObject process in processes)
+            {
+                Console.WriteLine(process.ToString());
+            }
+        }
+    }
+}
